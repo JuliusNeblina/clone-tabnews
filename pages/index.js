4726a2d1983a286e855fb24fbf9936d0b61f93b4
JuliusNeblina <167392897+JuliusNeblina@users.noreply.github.com>
2025-04-30 15:34:59 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Aula do curso.dev</h1>
+  return <h1>Aula do curso.dev</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste2");
 }
 
 export default Home;
