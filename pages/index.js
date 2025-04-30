28b05d942f188424dd94a4110b907164211a6a7e
JuliusNeblina <167392897+JuliusNeblina@users.noreply.github.com>
2025-04-30 15:46:21 +0000

    adiciona `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Aula do curso.dev</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("teste2");
-}
-
 export default Home;
